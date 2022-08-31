5f2c1569cc869c82aca5fd9ed0c94d3bcb4064e0
kode <105132310+kodescripts@users.noreply.github.com>
2022-08-31 15:23:27 +0200

    Create stumble-buddies-admin.lua

---
+++ stumble-buddies-admin.lua
@@ -0,0 +1,10 @@
+local Admin = game:GetService("Players").LocalPlayer.PlayerGui.Admin
+local OldIndex = nil
+OldIndex = hookmetamethod(game, "__index", function(s, k)
+    if not checkcaller() and s == Admin and k == "Enabled" then
+        return false
+    end
+    return OldIndex(s, k)
+end)
+wait()
+game:GetService("Players").LocalPlayer.PlayerGui.Admin.Enabled = true
